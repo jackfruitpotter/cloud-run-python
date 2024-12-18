b30d86fab586164b9993d576ce6ecfcbb478e1cc
LEW Kuo Shian <kslew@starhub.com>
2024-12-18 23:40:21 +0800

    add date to test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World from Cloud Run!'
+    return 'Hello, World from Cloud Run 2024-12-18!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
